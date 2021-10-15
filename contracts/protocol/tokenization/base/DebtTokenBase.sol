de4c721fae8a95d19c1223ac9c51431ee9a57767
SmartCoding51 <topstar51@outlook.com>
2021-10-15 08:46:34 +0700

    refactor

--- contracts/protocol/tokenization/base/DebtTokenBase.sol
+++ contracts/protocol/tokenization/base/DebtTokenBase.sol
@@ -2,6 +2,7 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 import {ILendingPool} from '../../../interfaces/ILendingPool.sol';
+import {ICreditDelegationToken} from '../../../interfaces/ICreditDelegationToken.sol';
 import {VersionedInitializable} from '../../libraries/upgradeability/VersionedInitializable.sol';
 import {IncentivizedERC20} from '../IncentivizedERC20.sol';
 import {Errors} from '../../libraries/helpers/Errors.sol';
@@ -12,7 +13,8 @@ import {Errors} from '../../libraries/helpers/Errors.sol';
 
 abstract contract DebtTokenBase is
   IncentivizedERC20('DEBTTOKEN_IMPL', 'DEBTTOKEN_IMPL', 0),
-  VersionedInitializable
+  VersionedInitializable,
+  ICreditDelegationToken
 {
   mapping(address => mapping(address => uint256)) internal _borrowAllowances;
 
