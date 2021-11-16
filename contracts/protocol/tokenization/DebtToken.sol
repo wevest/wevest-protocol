496493338c4a1c37a21f21c5d8e7b2661a0bd4f4
keinakano415 <kei.nakano@wevest.ai>
2021-11-17 03:05:56 +0700

    refactor

--- contracts/protocol/tokenization/DebtToken.sol
+++ contracts/protocol/tokenization/DebtToken.sol
@@ -193,7 +193,7 @@ contract DebtToken is IDebtToken, DebtTokenBase {
   }
 
   /**
-   * @dev Returns the address of the underlying asset of this aToken (E.g. WETH for aWETH)
+   * @dev Returns the address of the underlying asset of this aToken (E.g. WETH for wvWETH)
    **/
   function UNDERLYING_ASSET_ADDRESS() public view returns (address) {
     return _underlyingAsset;
